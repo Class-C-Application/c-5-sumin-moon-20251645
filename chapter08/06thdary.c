466755c2be7cab0c63149b3c1db844c5d80ec0ab
sumin-moon-20251645 <sumin1027@dongyang.ac.kr>
2026-04-01 02:20:31 +0000

    6

--- chapter08/06thdary.c
+++ chapter08/06thdary.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
-
+#define ROWSIZE 4
+#define COLSIZE 2
 // ROWSIZE라는 이름으로 정수 4를, COLSIZE라는 이름으로 정수 2를 나타내는 매크로 상수를 정의하세요
 
 int main(void)
 {
 	// 3차원 배열 초기화, 첫 번째 크기는 지정하지 않을 수 있음
+	int score[][ROWSIZE][COLSIZE] = {
+		{{95,85}, {85,83}, {92,75}, {90,88}},
+		{{88,77}, {72,95}, {88,92}, {93,83}}
+	};
+
+	for (int i = 0; i<2; i++)
+	{
+		if (i == 0)
+			printf("[강좌 1]");
+		else
+			printf("[강좌 2]");
+		printf("%11s%7s\n", "중간", "기말");
+
+		for (int j = 0; j<ROWSIZE;j++)
+		{
+			printf("%10s%2d", "학생", j + 1);
+			for (int k = 0; k<COLSIZE; k++)
+				printf("%6d ", score[i][j][k]);
+			printf("\n");
+		}
+		printf("\n");
+	}
 	// 크기가 [][ROWSIZE][COLSIZE]인 int형 3차원 배열 score를 아래 값으로 초기화하세요:
 	// 강좌1: {95,85}, {85,83}, {92,75}, {90,88}
 	// 강좌2: {88,77}, {72,95}, {88,92}, {93,83}
